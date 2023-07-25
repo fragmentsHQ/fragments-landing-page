62201d00cb4ff4899a2b7f529120eb861f8478b3
Ketan <krotusppp@gmail.com>
2023-07-25 23:15:27 +0530

    fix: add header

---
+++ components/navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white fixed top-0 h-[10vh] rounded-b-[36px] w-full">
+      <div className="container h-full w-full ">
+        <h1 className="text-[42px] font-bold bg-text-gradient w-fit bg-clip-text">
+          Fragments
+        </h1>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans"],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "text-gradient":
+          "radial-gradient(103.63% 203.27% at 56.5% 37.9%, #002366 0%, #000000 100%)",
       },
     },
   },
   plugins: [],
-}
+};
