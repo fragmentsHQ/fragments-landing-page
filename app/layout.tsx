b626f09476c5956a59d7e4441c37eb989b38f227
Ketan <krotusppp@gmail.com>
2023-08-06 21:35:31 +0530

    fix: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Plus_Jakarta_Sans } from "next/font/google";
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-jakarta",
 });
 
 export const metadata: Metadata = {
@@ -19,22 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@200;300;400;500&display=swap"
-        rel="stylesheet"
-      />
+    <html lang="en" className={jakarta.className}>
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💠</text></svg>"
       />
-      <body className={jakarta.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
