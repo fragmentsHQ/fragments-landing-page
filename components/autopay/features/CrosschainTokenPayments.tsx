217865e819e63c4c5b3031c783c5d98226e66774
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-08-08 23:51:11 +0530

    fix: add amount

--- components/autopay/features/CrosschainTokenPayments.tsx
+++ components/autopay/features/CrosschainTokenPayments.tsx
@@ -7,7 +7,7 @@ const CrossChainTokenPayments = () => {
       <div className="relative lg:max-w-[864px] md:block hidden md:max-w-[633px] md:h-[110px] w-full mt-10 mx-auto lg:h-[140px]">
         <Image src={"/images/steps.png"} fill alt="Steps" />
       </div>
-      <div className="relative lg:max-w-[864px] lg:block hidden md:max-w-[633px] md:h-[110px] w-full mt-10 mx-auto lg:h-[244px]">
+      <div className="relative lg:max-w-[864px] lg:block hidden md:max-w-[633px] md:h-[110px] w-full mt-10 mx-auto rounded-lg overflow-hidden lg:h-[294px]">
         <Image src={"/images/tokentable.png"} fill alt="Steps" />
       </div>
       <div className="relative lg:max-w-[864px] lg:hidden md:block hidden md:max-w-[633px] md:h-[248px] w-full mt-10 mx-auto lg:h-[244px]">
